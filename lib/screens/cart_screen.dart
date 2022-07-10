0ac061d977e4f1e633c77da9d1bbd37899aaa5b9
Rebecca <ry@nablaflow.no>
2022-07-10 22:10:38 +0200

    delete products from card implimented

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -45,6 +45,7 @@ class Cartscreen extends StatelessWidget {
             child: ListView.builder(
           itemBuilder: (context, index) => CartItem(
               id: cart.items.values.toList()[index].id,
+              productId: cart.items.keys.toList()[index],
               title: cart.items.values.toList()[index].title,
               quantity: cart.items.values.toList()[index].quantity,
               price: cart.items.values.toList()[index].price),
