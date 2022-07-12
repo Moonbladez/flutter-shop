bf16a45c7efe8c1882d85f910211903e32a68dad
Rebecca <ry@nablaflow.no>
2022-07-12 08:57:27 +0200

    added snackbar info when item is added to cart

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -41,6 +41,17 @@ class ProductItem extends StatelessWidget {
           trailing: IconButton(
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                action: SnackBarAction(
+                  label: "Undo",
+                  onPressed: () {
+                    cart.removeSingleItem(product.id);
+                  },
+                ),
+                content: Text("Item added to cart"),
+                duration: Duration(seconds: 2),
+              ));
             },
             icon: Icon(
               Icons.shopping_cart,
