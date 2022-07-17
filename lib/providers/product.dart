644b3cf7d5a3fd99128d7fcc2a6dead2647a8987
Rebecca <ry@nablaflow.no>
2022-07-17 11:47:58 +0200

    setting favourite now reflects in server aswell

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import "package:flutter/foundation.dart";
+import "package:http/http.dart" as http;
 
 class Product with ChangeNotifier {
   final String description;
@@ -17,8 +20,27 @@ class Product with ChangeNotifier {
     @required this.title,
   });
 
-  void toggleFavouriteStatus() {
+  void _setFavouriteValue(bool newValue) {
+    isFavourite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavouriteStatus() async {
+    final oldStatus = isFavourite;
     isFavourite = !isFavourite;
     notifyListeners();
+    final url = Uri.https(
+        "flutter-shop-af56d-default-rtdb.europe-west1.firebasedatabase.app",
+        "/products/${id}.json");
+
+    try {
+      var response = await http.patch(url,
+          body: json.encode({"isFavourite": isFavourite}));
+      if (response.statusCode >= 400) {
+        _setFavouriteValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavouriteValue(oldStatus);
+    }
   }
 }
