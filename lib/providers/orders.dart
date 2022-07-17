404597fd3fc69e642f0696a849aae499b4b6ddf1
Rebecca <ry@nablaflow.no>
2022-07-17 15:28:13 +0200

    send orders now sent to firebase

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,4 +1,7 @@
+import "dart:convert";
+
 import 'package:flutter/material.dart';
+import "package:http/http.dart" as http;
 
 import "./cart.dart";
 
@@ -22,13 +25,30 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final url = Uri.https(
+        "flutter-shop-af56d-default-rtdb.europe-west1.firebasedatabase.app",
+        "/orders.json");
+    final timestamp = DateTime.now().toIso8601String();
+    final response = await http.post(url,
+        body: json.encode({
+          "amount": total,
+          "dateTime": timestamp,
+          "products": cartProducts
+              .map((cartProduct) => {
+                    "id": cartProduct.id,
+                    "price": cartProduct.price,
+                    "quantity": cartProduct.quantity,
+                    "title": cartProduct.title,
+                  })
+              .toList()
+        }));
     _orders.insert(
         0,
         OrderItem(
             amount: total,
             dateTime: DateTime.now(),
-            id: DateTime.now().toString(),
+            id: json.decode(response.body)["name"],
             products: cartProducts));
   }
 
